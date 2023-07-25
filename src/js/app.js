d9ad8a7faa40d54a5fc2e246350caff466c8d5cd
Joulence <4erviken@gmail.com>
2023-07-25 17:30:08 +0300

    Created Traaffic block

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import * as isWebP from "./modules/isWeb.js";
 /* import * as menu from "./modules/menu.js";
 import * as date from "./modules/date.js"; */
 
-
 isWebP.isWebP();
 /* menu.onclick();
 date.thisDate(); */
